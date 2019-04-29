742efccbde7832c768945783e123661c3cd5d039
xiaobo <996233002@qq.com>
2019-04-29 21:34:18 +0800

    first

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
+
+import Cookies from 'js-cookie'
+
+//去除浏览器默认样式
+import 'normalize.css/normalize.css';
+
+import Element from 'element-ui';
+
+//element-ui默认样式
+import './styles/element-variable.scss'
+
 import App from './App.vue'
+import store from './store'
+import router from './router'
+
+import i18n from './lang'
+import './plugins/element.js'
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium',
+  i18n: (key, value) => i18n.t(key, value)
+})
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
+  i18n,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/settings.js
@@ -0,0 +1,35 @@
+module.exports = {
+    title: 'Vue Admin Template',
+
+    /**
+     * @type {boolean} true | false
+     * @description 是否显示设置右面板
+     */
+    showSettings: true,
+
+    /**
+     * @type {boolean} true | false
+     * @description 是否需要tagsView
+     */
+    tagsView: true,
+
+    /**
+     * @type {boolean} true | false
+     * @description 是否填充头部
+     */
+     fixedHeader: false,
+
+    /**
+     * @type {boolean} true | false
+     * @description 是否在侧边栏显示logo
+     */
+    sidebarLogo: false,
+
+    /**
+     * @type {string | array} 'production' | ['production', 'development']
+     * @description Need show err logs component.
+     * The default is only used in the production env
+     * If you want to also use it in dev, you can pass ['production', 'development']
+     */
+    errorLog: 'production'
+}
